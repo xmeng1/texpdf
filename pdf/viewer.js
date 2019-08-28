c36ee183c57c734b0a29f1b5503e268552fbf942
Xin Meng <x.meng@outlook.com>
2019-08-28 08:13:59 +0100

    update index pdf and default pdf for viewer

--- pdf/viewer.js
+++ pdf/viewer.js
@@ -3482,7 +3482,7 @@ var defaultOptions = {
   },
   defaultUrl: {
     // value: 'compressed.tracemonkey-pldi-09.pdf',
-    value: 'professional_cv_latest.pdf',
+    value: 'professional_pdf_latest.pdf',
     kind: OptionKind.VIEWER
   },
   defaultZoomValue: {
